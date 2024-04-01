4061dfcda6a673e9e3681958039466e4ff0d8162
hayatsoma5 <hayatsoma5@gmail.com>
2024-03-31 21:26:22 -0400

    code created

---
+++ week-2/project02-02/project02-02.js
@@ -0,0 +1,21 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Hayat Soma
+      Date:3/31/2024
+
+      Filename: project02-02.js
+ */
+function verifyForm() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  if (name && email && phone) {
+    window.alert("Thank you!");
+} else {
+    window.alert("Please fill in all fields");
+}
+}
+document.getElementById("submit").addEventListener("click", verifyForm);
